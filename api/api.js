290c5e3083c546902bc954450eaecd36e5637824
Gi0Vaa <100801222+Gi0Vaa@users.noreply.github.com>
2024-03-14 14:56:46 +0100

    get and post plants in a greenhouse

--- api/api.js
+++ api/api.js
@@ -324,6 +324,42 @@ app.get('/mapgreenhouses/:email', (req, res) => {
     });
 });
 
+//MapPlant
+app.get('/mapplants/:greenhouse_id', (req, res) => {
+    const id = req.params.greenhouse_id;
+    con.query('SELECT * FROM garden_plant WHERE greenhouse_id = ?', [id], (err, result, fields) => {
+        if (err) {
+            return res.status(500).json({
+                code: 500,
+                message: "Query failed"
+            });
+        }
+        if (result.length === 0) {
+            return res.status(404).json({
+                code: 404,
+                message: "Not found"
+            });
+        }
+        return res.status(200).json(result);
+    });
+});
+
+app.post('/mapplants', (req, res) => {
+    const map = req.body;
+    con.query('INSERT INTO garden_plant_greenhouse (greenhouse_id, plant_id ) VALUES (?, ?)', [map.greenhouse_id, map.plant_id ], (err, result, fields) => {
+        if (err) {
+            return res.status(500).json({
+                code: 500,
+                message: "Query failed"
+            });
+        }
+        return res.status(201).json({
+            code: 201,
+            message: "Created"
+        });
+    });
+});
+
 
 
 
