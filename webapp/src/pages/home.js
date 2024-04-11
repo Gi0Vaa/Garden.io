418d35357fdbb5367dc16f1412a0d65f13a8ea72
Nicolò <nicolo.spampa@gmail.com>
2024-04-11 19:41:11 +0200

    logout automatico

--- webapp/src/pages/home.js
+++ webapp/src/pages/home.js
@@ -9,30 +9,41 @@ import GreenhouseCard from '../components/greenhouseCard';
 function Home() {
     const email = localStorage.getItem('email');
     const [greenhouses, setGreenhouses] = useState([]);
-    
+
     useEffect(() => {
         document.title = 'Home | Garden.io';
 
-        axios.get(`${process.env.REACT_APP_API_URL}/users/${email}/greenhouses`)
-        .then(response => {
-            setGreenhouses(response.data);
-        })
-        .catch(err => {
-            if(err.response.data.code === 404){
-                window.location.href = '/start';
-            }
-        });
+        axios.get(`${process.env.REACT_APP_API_URL}/users/${email}`)
+            .then(response => {
+                axios.get(`${process.env.REACT_APP_API_URL}/users/${email}/greenhouses`)
+                    .then(response => {
+                        setGreenhouses(response.data);
+                    })
+                    .catch(err => {
+                        if (err.response.data.code === 404) {
+                            window.location.href = '/start';
+                        }
+                    });
+            })
+            .catch(err => {
+                if (err.response.data.code === 404) {
+                    localStorage.clear();
+                    window.location.href = '/register';
+                }
+            });
+
+
     }, [email]);
-    
+
 
 
     return (
         <React.Fragment>
-            <Header index={0}/>
+            <Header index={0} />
             <div className='mt-14 grid md:grid-cols-4 grid-cols-1 p-3'>
                 <div></div>
                 <div className='md:col-span-2 grid md:grid-cols-2 2xl:grid-cols-3 gap-2' id='greenhouses'>
-                    {greenhouses.map((g, index) => 
+                    {greenhouses.map((g, index) =>
                         <Link to={'/greenhouse'} state={g} key={index}>
                             <GreenhouseCard key={index} data={g} />
                         </Link>
