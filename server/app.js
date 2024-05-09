5bd59eac550bdb94c652698995d4686099f7d4aa
Nicolò <nicolo.spampa@gmail.com>
2024-05-09 19:28:01 +0200

    only localhost can access

--- server/app.js
+++ server/app.js
@@ -7,12 +7,21 @@ app.use(cors());
 
 const port = 3001;
 
+//only localhost can access
+app.use((req, res, next) => {
+    if(req.hostname === 'localhost'){
+        next();
+    }
+    else{
+        res.status(403).send('Forbidden');
+    }
+});
+
 //log delle richieste
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.path} dal client ${req.ip}:${req.socket.remotePort}`);
     next();
 });
-
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
