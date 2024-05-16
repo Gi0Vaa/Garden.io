4c159535bfc920c1cf21a2e92afd27ab50a94514
Nicolò <nicolo.spampa@gmail.com>
2024-05-16 17:30:23 +0200

    name and surname bug fix

--- server/routes/auth/passport.js
+++ server/routes/auth/passport.js
@@ -11,23 +11,25 @@ passport.use(
         clientSecret: process.env.CLIENT_SECRET,
         callbackURL: process.env.CALLBACK_URI,
         passReqToCallback: true
-    },(request, accessToken, refreshToken, profile, done) => {
+    }, (request, accessToken, refreshToken, profile, done) => {
         db.get("SELECT * FROM user WHERE g_id = ?", [profile.id], (err, user) => {
-            if(err){
+            if (err) {
                 return done(err);
             }
-            if(user){
+            if (user) {
                 return done(null, user);
             }
-            else{
-                db.run("INSERT INTO user (email, g_id, name, surname, pfp, role) VALUES (?, ?, ?, ?, ?, ?)", [profile.emails[0].value, profile.id, profile.name.givenName, profile.name.familyName, profile.photos[0].value, "user"], (err) => {
-                    if(err){
-                        return done(err);
-                    }
-                    else{
-                        return done(null, {email: profile.emails[0].value, g_id: profile.id, name: profile.name.givenName, surname: profile.name.familyName, pfp: profile.photos[0].value, role: this.user});
-                    }
-                });
+            else {
+                db.run("INSERT INTO user (email, g_id, name, surname, pfp, role) VALUES (?, ?, ?, ?, ?, ?)",
+                    [profile.emails[0].value, profile.id, profile.name.givenName || "", profile.name.familyName || "", profile.photos[0].value, "user"],
+                    (err) => {
+                        if (err) {
+                            return done(err);
+                        }
+                        else {
+                            return done(null, { email: profile.emails[0].value, g_id: profile.id, name: profile.name.givenName, surname: profile.name.familyName, pfp: profile.photos[0].value, role: this.user });
+                        }
+                    });
             }
         });
     })
